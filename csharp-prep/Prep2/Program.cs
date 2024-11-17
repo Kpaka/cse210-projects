2d328a600d0e5931f7cc40d78dc11de482ea8dae
kpaka <skpaka8@gmail.com>
2024-11-17 21:26:29 +0000

    cse9210-projects

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        Console.Write("What is your score?: ");
+        string gradeOne = Console.ReadLine();
+       int x = int.Parse(gradeOne);
+       /*int y = 90;*/
+
+       if (x >= 90)
+       {
+            Console.WriteLine("Congratulations, You got A!");
+       }
+       else if (x >= 80)
+       {
+            Console.WriteLine("Congratulations, You got B");
+       }
+       else if (x >= 70)
+       {
+            Console.WriteLine("Congratulation, You got C");
+       }
+       else if (x >= 60)
+       {
+            Console.WriteLine("You got D!, this's not a encouraging. Study hard");
+       }
+       else if (x < 60)
+       {
+         Console.WriteLine("You need to pay attention to your study, so you can get a better grade");
+       }
+       else
+       {
+            Console.WriteLine("Fail");
+       }
     }
+
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+          Random randomGenerator = new Random();
+          int magicWord = randomGenerator.Next(1, 101);
+
+          int guess = -1;
+
+          while (guess != magicWord )
+          {
+               Console.Write("What is your guess? ");
+               guess = int.Parse(Console.ReadLine());
+          }
+          if (magicWord > guess)
+          {
+               Console.WriteLine("Higher");
+          }
+          else if (magicWord < guess)
+          {
+               Console.WriteLine("Lower");
+          }
+          else
+          {
+               Console.WriteLine("You win!");
+          }
+              
     }
 }
